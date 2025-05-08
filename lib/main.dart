addd27ef58b0d04de70473232a1b2e5b9b1262f4
Marten Meichsner <meichsner.marten@proton.me>
2025-05-08 10:09:00 +0200

    added skip and continue button

--- lib/main.dart
+++ lib/main.dart
@@ -128,6 +128,18 @@ class _EnableCalendarsScreenState extends State<EnableCalendarsScreen> {
     {'name': 'Calendar 4', 'enabled': true},
     {'name': 'Calendar 5', 'enabled': false},
     {'name': 'Calendar 6', 'enabled': false},
+    {'name': 'Calendar 7', 'enabled': true},
+    {'name': 'Calendar 8', 'enabled': false},
+    {'name': 'Calendar 9', 'enabled': true},
+    {'name': 'Calendar 10', 'enabled': false},
+    {'name': 'Calendar 11', 'enabled': true},
+    {'name': 'Calendar 12', 'enabled': false},
+    {'name': 'Calendar 13', 'enabled': true},
+    {'name': 'Calendar 14', 'enabled': false},
+    {'name': 'Calendar 15', 'enabled': true},
+    {'name': 'Calendar 16', 'enabled': false},
+    {'name': 'Calendar 17', 'enabled': true},
+    {'name': 'Calendar 18', 'enabled': false},
   ];
 
   @override
@@ -141,44 +153,64 @@ class _EnableCalendarsScreenState extends State<EnableCalendarsScreen> {
         backgroundColor: Colors.brown[100],
       ),
 
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //TEXT
-            const Text(
-              "Please enable Calendars you want to be colored by CoCo",
-              style: TextStyle(color: Colors.white, fontSize: 21, fontFamily: 'NewComputerModern'),
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                //TEXT
+                const Text(
+                  "Please enable calendars you want to be colored by CoCo",
+                  style: TextStyle(color: Colors.white, fontSize: 21, fontFamily: 'NewComputerModern'),
+                ),
+
+                //SPACING
+                const SizedBox(height: 20),
+
+                //SCROLLABLE LIST
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _calendars.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: Switch(
+                          value: _calendars[index]['enabled'],
+                          onChanged: (bool newValue) {
+                            setState(() {
+                              _calendars[index]['enabled'] = newValue;
+                            });
+                          },
+                          activeColor: Colors.green,
+                          inactiveTrackColor: Colors.brown[100],
+
+                        ),
+                        title: Text(
+                          _calendars[index]['name'],
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 18, fontFamily: 'NewComputerModern'),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            //SCROLLABLE LIST
-            Expanded(
-              child: ListView.builder(
-                itemCount: _calendars.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: Switch(
-                      value: _calendars[index]['enabled'],
-                      onChanged: (bool newValue) {
-                        setState(() {
-                          _calendars[index]['enabled'] = newValue;
-                        });
-                      },
-                      activeColor: Colors.green,
-                      inactiveTrackColor: Colors.grey,
-                    ),
-                    title: Text(
-                      _calendars[index]['name'],
-                      style: const TextStyle(
-                          color: Colors.white, fontSize: 18, fontFamily: 'NewComputerModern'),
-                    ),
-                  );
-                },
-              ),
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: Row(
+              children: [
+                TextButton(onPressed: (){},
+                    child: const Text("skip", style: TextStyle(color: Colors.white, fontSize: 18, fontFamily: 'NewComputerModern'))),
+                ElevatedButton(onPressed: (){}, child: const Text("continue", style: TextStyle(color: Colors.black, fontSize: 18, fontFamily: 'NewComputerModern')), style: ElevatedButton.styleFrom(backgroundColor: Colors.brown[100]))
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
